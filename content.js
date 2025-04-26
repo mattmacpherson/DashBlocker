ebd3a066c659ba991026a41fb362f9ff79ebca6f
mattmacpherson <matt@edabit.com>
2025-04-26 17:18:44 -0400

    Initial setup of Dash Nuke extension with manifest and content script

---
+++ content.js
@@ -0,0 +1,10 @@
+console.log("Dash Nuke: Content script loaded and running on", window.location.href);
+
+// Placeholder for future logic
+function initializeDashNuke() {
+    console.log("Dash Nuke: Initializing...");
+    // We will add tweet detection logic here in the next phase
+}
+
+// Run initialization logic
+initializeDashNuke();
